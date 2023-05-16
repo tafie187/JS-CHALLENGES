c821b1864986541a5923af20881d3ce13429b6d0
tafie187 <127634792+tafie187@users.noreply.github.com>
2023-05-16 04:45:59 +0200

    Add files via upload

---
+++ Prime Numbers/index.js
@@ -0,0 +1,20 @@
+//defining the range within which you want to find prime numbers.
+const startRange = 1;
+const endRange = 50;
+
+for (let i = startRange; (i = endRange); i++);
+{
+  // Code to check if i is a prime number
+  let isPrime = true;
+  //This variable will be used to determine if the current number is a prime number.
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    // Code to check if i is divisible by j
+    if (i % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    console.log(i + " is a prime number.");
+  }
+}
