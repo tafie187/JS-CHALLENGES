8f42319aa7cff2cedb7f9b828d4c1faa7550d389
Tafie <tafadzwachijarira96@gmail.com>
2023-05-17 15:33:13 +0200

    done

--- Prime Numbers/index.js
+++ Prime Numbers/index.js
@@ -1,38 +1,23 @@
 // //defining the range within which you want to find prime numbers.
-// const startRange = 2;
-// const endRange = 50;
-
-// for (let i = startRange; i <= endRange; i++);
-// {
-//   // Code to check if i is a prime number
-//   let isPrime = true;
-//   //This variable will be used to determine if the current number is a prime number.
-//   for (let j = 2; j <= Math.sqrt(i); j++) {
-//     // Code to check if i is divisible by j
-//     if (i % j === 0) {
-//       isPrime = false;
-//       break;
-//     }
-//   }
-//   if (isPrime) {
-//     console.log(i + " is a prime number.");
-//   }
-// }
-
-
 const startRange = 2;
 const endRange = 50;
-for (let i = startRange; i <= endRange; i++) {
-  // Code to check if i is a prime number
-let isPrime = true;
-for (let j = 2; j <= Math.sqrt(i); j++) {
-  // Code to check if i is divisible by j
 
-if (i % j === 0) {break;
-  isPrime = false;
-}
-}
-if (!isPrime) {
-  console.log(i + " is a prime number.");
-}
+console.log(`The prime numbers between ${startRange} and ${endRange} are:`);
+
+// looping from startRange to higherNumber
+for (let i = startRange; i <= endRange; i++) {
+    let flag = 0;
+
+    // looping through 2 to endRange and High
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
 }
